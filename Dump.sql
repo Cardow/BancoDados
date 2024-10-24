be2dafcd35bf68bd1ba13925145e375cf35268d6
Ricardo <ricardocastello89@gmail.com>
2024-10-24 16:36:11 -0300

    Teste

--- Dump.sql
+++ Dump.sql
@@ -5,3 +5,10 @@ CREATE TABLE
         'descricao' text,
         'Pontos' int
     );
+
+CREATE TABLE
+    Categorias (
+        'nome' VARCHAR(50),
+        'descricao' text,
+        'Pontos' int
+    );
