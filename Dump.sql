2da4cecf7d261e35faadbff6176cae3c4c2acffa
Ricardo <ricardocastello89@gmail.com>
2024-10-25 00:52:58 -0300

    Formatei direito agora

--- Dump.sql
+++ Dump.sql
@@ -1,34 +1,27 @@
 --Excluir Tabelas
+
 DROP TABLE IF EXISTS Categorias;
-
 DROP TABLE IF EXISTS Subcategorias;
-
 DROP TABLE IF EXISTS Produtos;
-
 DROP TABLE IF EXISTS Clientes;
-
 DROP TABLE IF EXISTS Pedidos;
-
 DROP TABLE IF EXISTS ItensPedidos;
-
 DROP TABLE IF EXISTS Pagamentos;
-
 DROP TABLE IF EXISTS Entregas;
-
 DROP TABLE IF EXISTS Funcionarios;
-
 DROP TABLE IF EXISTS Fornecedores;
-
 DROP TABLE IF EXISTS Estoque;
 
 --Tabela de Categorias
-CREATE TABLE
-    categorias (nome VARCHAR(50), descricao text, pontos int);
+CREATE TABLE categorias (
+    nome VARCHAR(50),
+    descricao text,
+    pontos int
+);
 
 INSERT INTO
     categorias (nome, descricao, pontos)
-VALUES
-    (
+VALUES (
         "Medicamentos",
         "Medicamentos de prescrição e de venda livre para o tratamento de diversas condições médicas",
         30
